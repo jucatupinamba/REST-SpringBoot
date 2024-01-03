f671a7bce9b9d1c2dbf27fd13e976adf2bfcec66
Juarez Martins <juniorbmx13@gmail.com>
2024-01-03 07:47:43 -0300

    feat: exception handling

--- src/main/java/com/restspringboot/RestSpringboot/MathController.java
+++ src/main/java/com/restspringboot/RestSpringboot/MathController.java
@@ -1,5 +1,6 @@
 package com.restspringboot.RestSpringboot;
 
+import com.restspringboot.RestSpringboot.exception.UnsupportedMathOperationException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -15,7 +16,7 @@ public class MathController {
             @PathVariable(value = "numberTwo") String numberTwo)
         throws Exception{
         if(!isNumeric(numberOne) || !isNumeric(numberTwo) ){
-            throw new Exception();
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
         }
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
@@ -24,13 +25,13 @@ public class MathController {
         if(strNumber == null) return 0D;
         String number = strNumber.replaceAll(",", ".");
         if(isNumeric(number)) return Double.parseDouble(number);
-        return null;
+        return 0D;
     }
 
     private boolean isNumeric(String strNumber) {
         if(strNumber == null) return false;
         String number = strNumber.replaceAll(",", ".");
-        return number.matches("[-+]?[0-9]*\\.?[0-9]");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
 
     }
 }
